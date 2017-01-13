fdbc506962476bd30ecfb97ce2144ff127c33a51
Ricardo Tucker <ricardo@ricardos-imac.home>
2017-01-12 20:48:56 -0500

    fixed resizing/grid again

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,18 +1,11 @@
 function loadMasonry() {
-  var $grid = $('.grid');
+  var $grid = $('#grid');
   $grid.imagesLoaded(function() {
     $grid.masonry({
       fitWidth: true,
       gutter: 20,
       stagger: 30,
       itemSelector: '.grid-item',
-
-      isAnimated: true,
-      animationOptions: {
-        duration: 750,
-        easing: 'linear',
-        queue: false
-      }
     });
   });
 
