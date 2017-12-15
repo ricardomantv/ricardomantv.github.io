ef5053cddb6f2de1064b68d187b4445f09d4d94b
Ricardo Tucker <ricardomantv@gmail.com>
2017-12-15 15:11:07 -0500

    massive update, single page portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,13 +1,33 @@
+var $grid = $('#grid');
 function loadMasonry() {
-  var $grid = $('#grid');
   $grid.imagesLoaded(function() {
-    $grid.masonry({
-      fitWidth: true,
-      gutter: 20,
-      stagger: 50,
+    $grid.isotope({
       itemSelector: '.grid-item',
-      transitionDuration: '0.750s',
-      resize: true,
+      masonry: {
+        fitWidth: true,
+        gutter: 20,
+        stagger: 50,
+        transitionDuration: '0.750s',
+        resize: true,
+      }
     });
   });
 }
+
+// bind filter on click
+$('#filter-buttons').on('click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({filter: filterValue});
+});
+
+// change selected class on button
+$('#filter-buttons').each(function(i, buttonGroup){
+  var $buttonGroup = $(buttonGroup);
+  $buttonGroup.on('click', 'button', function() {
+    var $oldSelected = $buttonGroup.find('.btn-primary');
+    $oldSelected.removeClass('btn-primary');
+    $oldSelected.addClass('btn-secondary');
+    $(this).removeClass('btn-secondary');
+    $(this).addClass('btn-primary');
+  })
+});
